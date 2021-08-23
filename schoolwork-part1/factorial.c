6d1f569e270ea265cc338c1a94d712617297ba18
lopeztomas <lopeztomasenrique98@gmail.com>
2021-08-23 17:13:11 -0300

    feat: Factorial function finished.

--- schoolwork-part1/factorial.c
+++ schoolwork-part1/factorial.c
@@ -1,7 +1,14 @@
 #include "factorial.h"
 
 double factorialOf(int x) {
-    double factorial;
+    double factorial = x; // user's number assigned as initial value of its factorial.
 
-    return ;
+    if (x <= 1) {
+        factorial = 1;
+    } else {
+        for(int i = 1; i < x; i++) {
+            factorial *= (x - i);
+        }
+    }
+    return factorial;
 }
