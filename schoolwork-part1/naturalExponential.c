96d26a2b6d6fc36fb2b5d31dc89dee7c5ac13b0a
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-08-26 23:37:48 -0300

    feat: calculateNaturalExponential(), enterNumber() & enterTol() function prototypes added.

--- schoolwork-part1/naturalExponential.c
+++ schoolwork-part1/naturalExponential.c
@@ -1,6 +1,18 @@
 #include "naturalExponential.h"
 #include "factorial.h"
 
-float eNumber(int x, float tol) {
+double calculateNaturalExponential(int x, float tol) {
 
+}
+
+int enterNumber() {
+    int num;
+
+    return num;
+}
+
+float enterTol() {
+    float tol;
+
+    return tol;
 }
--- schoolwork-part1/naturalExponential.h
+++ schoolwork-part1/naturalExponential.h
@@ -1,6 +1,8 @@
 #ifndef NATURALEXPONENTIAL
 #define NATURALEXPONENTIAL
 
+double calculateNaturalExponential(int x, float tol);
 int enterNumber();
+float enterTol();
 
 #endif // NATURALEXPONENTIAL
