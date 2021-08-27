3c10b9442241feebfb4c3d830571111daba7af7d
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-08-26 23:43:45 -0300

    feat: enterTol() function completed. I could add conditional compilation to clean the console. Besides I created two defines: MAX_FLOAT & MIN_FLOAT with its values.

--- schoolwork-part1/naturalExponential.c
+++ schoolwork-part1/naturalExponential.c
@@ -1,6 +1,9 @@
 #include "naturalExponential.h"
 #include "factorial.h"
 
+#define MAX_FLOAT 1
+#define MIN_FLOAT 0.000001
+
 double calculateNaturalExponential(int x, float tol) {
 
 }
@@ -8,11 +11,18 @@ double calculateNaturalExponential(int x, float tol) {
 int enterNumber() {
     int num;
 
+    do {}
+
     return num;
 }
 
 float enterTol() {
     float tol;
+
+    do {
+        printf("Enter the desired tolerance [%d - %f]: ", MAX_FLOAT, MIN_FLOAT);
+        scanf(" %f", &tol);
+    } while( tol > MAX_FLOAT || tol < MIN_FLOAT);
 
     return tol;
 }
