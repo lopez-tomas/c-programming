bd61e6042bc63aa0e4b3ecbb23d65a8016e09b9e
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-08-27 00:07:13 -0300

    fix: stdio.h & stdlib.h libraries removed from factorial.h

--- schoolwork-part1/factorial.h
+++ schoolwork-part1/factorial.h
@@ -1,9 +1,6 @@
 #ifndef FACTORIAL_H
 #define FACTORIAL_H
 
-#include <stdio.h>
-#include <stdlib.h>
-
 double factorialOf(int x);
 int enterNumberToFactor(int maxNumberToFactor);
 void printFactoredNumber(int baseNumber, double factoredNumber, int maxFactoredNumber);
