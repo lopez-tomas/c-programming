850b663d1836861490c5946cee6a87f135d99fd7
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-08-27 01:10:12 -0300

    feat: numberForRoot() function completed & stdio.h library added.

--- schoolwork-part1/squareRoot.c
+++ schoolwork-part1/squareRoot.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 #include "squareRoot.h"
+
+double squareRootOf(int number, double tol) {
+
+}
+
+int numberForRoot() {
+    int num;
+
+    do {
+        printf("Enter a positive number [ >= 1 ]: ");
+        scanf(" %d", &num);
+    } while(num < 1);
+
+    return num;
+}
