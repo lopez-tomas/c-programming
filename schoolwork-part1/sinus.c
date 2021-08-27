07cfeb2f3296c03758c99bf81f8eed26b9d72bff
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-08-27 15:08:44 -0300

    feat: absolute() & sinusOf() function prototypes and developmente added.

--- schoolwork-part1/sinus.c
+++ schoolwork-part1/sinus.c
@@ -1,1 +1,25 @@
 #include "sinus.h"
+#include "naturalExponential.h"
+#include "factorial.h"
+
+double sinusOf(int number, double tol) {
+    int exponent = 3,
+        numberRaised;
+    double factoredNumber,
+           term = number;
+
+    do {
+        numberRaised = raiseNumber(number, exponent);
+        factoredNumber = factorialOf(exponent);
+
+        term -= numberRaised / factoredNumber;
+
+        exponent += 2;
+    } while( absolute(term) > tol );
+
+    return term;
+}
+
+double absolute(double value) {
+    return value *= -1;
+}
--- schoolwork-part1/sinus.h
+++ schoolwork-part1/sinus.h
@@ -1,6 +1,7 @@
 #ifndef SINUS_H
 #define SINUS_H
 
-
+double sinusOf(int number, double tol);
+double absolute(double value);
 
 #endif // SINUS_H
