59be6e96acd6d4c3cee2c194faa672fa9e512614
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-08-27 15:23:08 -0300

    feat: sinus.h included.

--- schoolwork-part1/main.h
+++ schoolwork-part1/main.h
@@ -8,6 +8,7 @@
 #include "factorial.h" /// Exercise 1
 #include "naturalExponential.h" /// Exercise 3
 #include "squareRoot.h" /// Exercise 4
+#include "sinus.h" /// Exercise 6
 
 void exerciseTitle(char message[]);
 char continueOperating();
