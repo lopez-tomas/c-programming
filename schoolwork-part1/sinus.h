9ab6ca62c4a044378d0d9a7069fb395ce48dd92a
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-08-27 16:17:56 -0300

    feat: Comment about absolute() function added.

--- schoolwork-part1/sinus.h
+++ schoolwork-part1/sinus.h
@@ -3,6 +3,6 @@
 
 double sinusOf(int number, double tol);
 int numberForSinus();
-double absolute(double value);
+double absolute(double value); /// I'm not using this function because I calculate the term independently.
 
 #endif // SINUS_H
