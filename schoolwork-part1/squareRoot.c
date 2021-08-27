13dde2fa2b9c8f707595b9670189ce86610f5d27
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-08-27 01:38:21 -0300

    fix: Function fixed. Now it works fine. Flag added to control difference between first and second term variables.

--- schoolwork-part1/squareRoot.c
+++ schoolwork-part1/squareRoot.c
@@ -5,12 +5,18 @@ double squareRootOf(int number, double tol) {
     double firstTerm,
            secondTerm = 1.0,
            difference;
+    int flag = 1;
 
     do {
         firstTerm = secondTerm;
         secondTerm = ( firstTerm + (number / firstTerm) ) / 2;
-        difference = secondTerm - firstTerm;
-    } while( difference < tol );
+        if (flag) {
+            difference = secondTerm - firstTerm;
+        } else {
+            difference = firstTerm - secondTerm;
+        }
+        flag = 0;
+    } while( difference > tol );
 
     return secondTerm;
 }
