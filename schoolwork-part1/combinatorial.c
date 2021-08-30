abbae531de542cf8cd63029a361d6fdc78a5417f
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-08-30 01:41:26 -0300

    feat(function): enterNumberM() developed.

--- schoolwork-part1/combinatorial.c
+++ schoolwork-part1/combinatorial.c
@@ -4,10 +4,22 @@ int enterNumberN() {
     int n;
 
     do {
-        printf("Enter an integer [ >= 0 ]: ");
+        printf("Enter an integer 'n' [ >= 0 ]: ");
         scanf(" %d", &n);
     } while( n < 0 );
 
     return n;
 }
+
+int enterNumberM(int n) {
+    int m;
+
+    do {
+        printf("Enter an integer 'm' [ >= %d ]: ", n);
+        scanf(" %d", &m);
+    } while ( m < n );
+
+    return m;
+}
+
 
