18f926cfed4e749aac6f8a75a614ce477c3ef94b
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-08-30 15:44:27 -0300

    feat: enterNaturalNumber() productWithSuccessiveSums() developed.

--- schoolwork-part1/successiveSums.c
+++ schoolwork-part1/successiveSums.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
 #include "successiveSums.h"
+
+int enterNaturalNumber() {
+    int num;
+
+    do {
+        printf("Enter a natural number ( >= 0 ): ");
+        scanf(" %d", &num);
+    } while (num < 0);
+
+    return num;
+}
+
+unsigned long long int productWithSuccessiveSums(int numA, int numB) {
+    int i;
+    unsigned long long int result = 0;
+
+    if (numA == 0 || numB == 0) {
+        return 0;
+    }
+    if (numA == 1) {
+        return numB;
+    }
+    if (numB == 1) {
+        return numA;
+    }
+
+    for(i = 0; i < numB; i++) {
+        result += numA;
+    }
+
+    return result;
+}
