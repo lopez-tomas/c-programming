0a797224f072691b80e78512a888928bcb03c87a
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-08-30 15:44:04 -0300

    feat: enterNaturalNumber() & productWithSuccessiveSums() function protoypes.

--- schoolwork-part1/successiveSums.h
+++ schoolwork-part1/successiveSums.h
@@ -1,6 +1,7 @@
 #ifndef SUCCESSIVESUMS_H
 #define SUCCESSIVESUMS_H
 
-
+int enterNaturalNumber();
+unsigned long long int productWithSuccessiveSums(int numA, int numB);
 
 #endif // SUCCESSIVESUMS_H
