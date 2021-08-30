405e4623c97431ba6e9254c525dd93ac2a9dd64f
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-08-30 12:52:28 -0300

    feat: isPerfectSquare() developed.

--- schoolwork-part1/fibonacci.c
+++ schoolwork-part1/fibonacci.c
@@ -1,1 +1,14 @@
 #include "fibonacci.h"
+
+int isPerfectSquare(int number) {
+    int answer = 0,
+        i;
+
+    for (i = 1; i < number; i++) {
+        if ( i*i == number ) {
+            answer = 1;
+            return answer;
+        }
+    }
+    return answer;
+}
