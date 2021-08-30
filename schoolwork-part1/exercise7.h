eac9990db593f2f83b3e423ccb7336b0aa1be7ed
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-08-30 14:50:53 -0300

    feat: numberClassification() & printClassification() prototypes added.

--- schoolwork-part1/exercise7.h
+++ schoolwork-part1/exercise7.h
@@ -1,6 +1,7 @@
 #ifndef EXERCISE7_H
 #define EXERCISE7_H
 
-
+int numberClassification(int number); // It returns 0 = Perfect, 1 = Deficient or 2 = Abundant
+void printClassification(int number, int classification);
 
 #endif // EXERCISE7_H
