1f4cf4b6991f30ac1516b0e4a3b8963fe24fee93
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-08-30 01:39:49 -0300

    feat(function): enterNumberN() developed.

--- schoolwork-part1/combinatorial.c
+++ schoolwork-part1/combinatorial.c
@@ -1,1 +1,13 @@
 #include "combinatorial.h"
+
+int enterNumberN() {
+    int n;
+
+    do {
+        printf("Enter an integer [ >= 0 ]: ");
+        scanf(" %d", &n);
+    } while( n < 0 );
+
+    return n;
+}
+
