3d2dc1c88ff4ca5f33c0880df8d157e0bd551fd1
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-08-31 10:32:21 -0300

    feat: enterNumberA() developed and <stdio.h> included.

--- schoolwork-part1/division.c
+++ schoolwork-part1/division.c
@@ -1,5 +1,15 @@
+#include <stdio.h>
 #include "division.h"
 
+
+int enterNumberA() {
+    int num;
+
+    printf("Enter number A (it could be 0): ");
+    scanf(" %d", &num);
+
+    return num;
+}
 
 int obtainQuotient(int numberA, int numberB) {
     if(numberA == 0) {
