052ec090b056088f091facfa7539f57093779b2d
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-08-31 13:34:45 -0300

    feat: DECIMAL_PART(x) macro & returnDecimalPart() function prototype defined to test macro.

--- schoolwork-part1/macros.h
+++ schoolwork-part1/macros.h
@@ -3,8 +3,10 @@
 
 #define ABS(x) (x) == 0 ? 0 : ((x) > 0 ? (x) : -(x))
 #define INT_PART(x) (int)(x)
+#define DECIMAL_PART(x) (x) - INT_PART(x)
 
 int toAbsoluteValue(int x);
 int returnIntPart(float x);
+float returnDecimalPart(float x);
 
 #endif // MACROS_H
