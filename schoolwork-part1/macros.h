e1f629c1d5302f9a58205cffb48e0d21d2808ea1
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-08-31 13:49:07 -0300

    feat: IS_MAYUS() & IS_MINUS() macros defined.

--- schoolwork-part1/macros.h
+++ schoolwork-part1/macros.h
@@ -4,6 +4,8 @@
 #define ABS(x) (x) == 0 ? 0 : ((x) > 0 ? (x) : -(x))
 #define INT_PART(x) (int)(x)
 #define DECIMAL_PART(x) (x) - INT_PART(x)
+#define IS_MAYUS(c) (INT_PART(c) >= 65 && INT_PART(c) <= 90) ? 1 : 0
+#define IS_MINUS(c) (INT_PART(c) >= 97 && INT_PART(c) <= 122) ? 1 : 0
 
 int toAbsoluteValue(int x);
 int returnIntPart(float x);
