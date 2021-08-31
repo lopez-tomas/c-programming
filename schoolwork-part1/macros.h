fc10ec55af9db79be7cd529a6d83f5b4ebbdaf1b
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-08-31 14:55:04 -0300

    feat: Minor changes on MACROS definitions.

--- schoolwork-part1/macros.h
+++ schoolwork-part1/macros.h
@@ -4,11 +4,11 @@
 #define ABS(x) (x) == 0 ? 0 : ((x) > 0 ? (x) : -(x))
 #define INT_PART(x) (int)(x)
 #define DECIMAL_PART(x) (x) - INT_PART(x)
-#define IS_NUMBER(c) (INT_PART(c) >= 48 && INT_PART(c) <= 57) ? 1 : 0
-#define IS_MAYUS(c) (INT_PART(c) >= 65 && INT_PART(c) <= 90) ? 1 : 0
-#define IS_MINUS(c) (INT_PART(c) >= 97 && INT_PART(c) <= 122) ? 1 : 0
-#define IS_LETTER(c) (IS_MAYUS(c) || IS_MINUS(c)) ? 1 : 0
-#define IS_WHITE(c) (c) == ' ' ? 1 : 0
+#define IS_NUMBER(c) (INT_PART(c) >= '0' && INT_PART(c) <= '9')
+#define IS_MAYUS(c) ((c) >= 'A' && (c) <= 'Z')
+#define IS_MINUS(c) ((c) >= 'a' && (c) <= 'z')
+#define IS_LETTER(c) (IS_MAYUS(c) || IS_MINUS(c))
+#define IS_WHITE(c) (c) == ' '
 #define TO_MAYUS(c) ( (c) >= 'a' && (c) <= 'z' ) ? ( ( (c) - 'a' ) + 'A' ) : (c)
 #define TO_MINUS(c) ( (c) >= 'A' && (c) <= 'Z' ) ? ( ( (c) - 'A' ) + 'a' ) : (c)
 
