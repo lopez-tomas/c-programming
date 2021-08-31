74e871cada523ffc0c9e3628d6e3e554a43a5c0c
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-08-31 18:54:05 -0300

    feat: printArray() and fillArray() functions added.

---
+++ vectors/vectors.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "vectors.h"
+
+void printArray(int* vec, int posAct, int tam) {
+    int i;
+    printf("%p\n\n", &vec);
+    for(i = 0; i < posAct; i++) {
+        printf("pos %d -> %d\n", i, *vec);
+        vec++;
+    }
+}
+
+int fillArray(int* vec, int posAct, int tam) {
+    int condition;
+
+    if (posAct == tam) {
+        return posAct;
+    }
+
+    vec += posAct;
+
+    printf("\nDo you want to fill the array (1: Yes/ 0: No): ");
+    scanf(" %d", &condition);
+    while (condition && posAct < tam) {
+        printf("\nEnter a number (-1 to exit): ");
+        scanf(" %d", vec);
+
+        vec++;
+        posAct++;
+
+        printf("\n\nDo you want to fill the array (1: Yes/ 0: No): ");
+        scanf(" %d", &condition);
+    }
+    return posAct;
+}
---
+++ vectors/vectors.h
@@ -0,0 +1,7 @@
+#ifndef VECTORS_H
+#define VECTORS_H
+
+void printArray(int* vec, int posAct, int tam);
+int fillArray(int* vec, int posAct, int tam);
+
+#endif // VECTORS_H
