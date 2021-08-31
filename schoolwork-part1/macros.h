c69a7269b285b0971a4e146de65899b7ae33ddf2
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-08-31 13:28:39 -0300

    feat: INT_PART() macro defined & returnIntPart() function prototype to test it.

--- schoolwork-part1/macros.h
+++ schoolwork-part1/macros.h
@@ -2,7 +2,9 @@
 #define MACROS_H
 
 #define ABS(x) (x) == 0 ? 0 : ((x) > 0 ? (x) : -(x))
+#define INT_PART(x) (int)(x)
 
 int toAbsoluteValue(int x);
+int returnIntPart(float x);
 
 #endif // MACROS_H
