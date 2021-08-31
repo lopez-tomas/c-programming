86affc665a00b217adb6f6e14b90f27fe20f37a4
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-08-31 13:57:37 -0300

    feat: IS_NUMBER() & IS_LETTER() macros defined.

--- schoolwork-part1/macros.h
+++ schoolwork-part1/macros.h
@@ -4,8 +4,10 @@
 #define ABS(x) (x) == 0 ? 0 : ((x) > 0 ? (x) : -(x))
 #define INT_PART(x) (int)(x)
 #define DECIMAL_PART(x) (x) - INT_PART(x)
+#define IS_NUMBER(c) (INT_PART(c) >= 48 && INT_PART(c) <= 57) ? 1 : 0
 #define IS_MAYUS(c) (INT_PART(c) >= 65 && INT_PART(c) <= 90) ? 1 : 0
 #define IS_MINUS(c) (INT_PART(c) >= 97 && INT_PART(c) <= 122) ? 1 : 0
+#define IS_LETTER(c) (IS_MAYUS(c) || IS_MINUS(c)) ? 1 : 0
 
 int toAbsoluteValue(int x);
 int returnIntPart(float x);
