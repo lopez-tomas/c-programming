7da01ac73a7df4dd997e32256bdf1bba6b51ec22
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-09-02 16:01:58 -0300

    feat: insertElementInto() function prototype added (it could change.

--- vectors/vectors.h
+++ vectors/vectors.h
@@ -3,5 +3,6 @@
 
 void printArray(int* vec, int posAct, int tam);
 int fillArray(int* vec, int posAct, int tam);
+void insertElementInto(int* vec, int element, int pos, int posAct, int tam);
 
 #endif // VECTORS_H
