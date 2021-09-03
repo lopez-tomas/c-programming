0f0f634214771698aee4fa5a2e24e1ad4024fe63
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-09-03 12:01:40 -0300

    feat: minor changes.

--- vectors/main.c
+++ vectors/main.c
@@ -8,15 +8,17 @@
 int main() {
     /// Exercise 22 variables - Insert element into array with a given position
     int testVec[10] = {3, 5, -2, 10, 200, 1, 15, -8, 9, 10},
-        cantElem = 10;
-    int elem = 0,
-        pos = 10,
+        cantElem = sizeof(testVec) / TAM_INT;
+    int elem = 10000,
+        pos = 6,
         result;
 
-    printf("testVec[] without changes: \n\n");
+    printf("testVec[]: \n\n");
     printArray(testVec, cantElem, sizeof(testVec) / TAM_INT);
-    result = insertElementInto(testVec, elem, pos, cantElem, sizeof(testVec) / TAM_INT);
-    printf("\n\ntestVec[] with changes: \n\n");
+
+    result = insertElementInto(testVec, elem, pos, cantElem, cantElem);
+
+    printf("\n\ntestVec[] with changes (or not): \n\n");
     printArray(testVec, cantElem, sizeof(testVec) / TAM_INT);
 
     return 0;
