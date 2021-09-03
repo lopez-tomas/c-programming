16f39309f9b287d7d79786b03e01b87f36534ace
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-09-03 14:10:56 -0300

    feat: insertElementoIntoOrderedVector() function prototype added.

--- vectors/vectors.h
+++ vectors/vectors.h
@@ -4,5 +4,6 @@
 void printArray(int* vec, int posAct, int tam);
 int fillArray(int* vec, int posAct, int tam);
 int insertElementInto(int* vec, int element, int pos, int posAct, int tam);
+void insertElementIntoOrderedVector(int* vec, int element, int tam);
 
 #endif // VECTORS_H
