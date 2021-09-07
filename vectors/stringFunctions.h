590f131f0aa3ecb14beb195a59240d1cbec2578d
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-09-07 09:48:38 -0300

    feat: Now, data type of given character is 'char' and not 'int'.

--- vectors/stringFunctions.h
+++ vectors/stringFunctions.h
@@ -4,7 +4,7 @@
 char* str_cpy(char* destiny, char* origin);
 unsigned int str_len(char* origin);
 char* str_cat(char* destiny, char* origin);
-char* str_chr(char* source, int character);
+char* str_chr(char* source, char character);
 char* str_rchr(char* source, char character);
 
 #endif // STRINGFUNCTIONS_H
