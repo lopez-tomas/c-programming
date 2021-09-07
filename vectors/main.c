1789e9a17ea7e6a9f253433646618e2735b3c7ad
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-09-07 01:15:23 -0300

    feat: Commenting last exercises and test for str_cat() function added.

--- vectors/main.c
+++ vectors/main.c
@@ -5,44 +5,55 @@
 
 #define TAM_INT sizeof(int)
 #define TAM_VEC 10
+#define SIZE 100
 
 int main() {
     /// Exercise 22 variables - Insert element into array with a given position
-    int testVec[TAM_VEC] = {10, 20, 30, 40},
-        numberElements = 4;
-    int elem = 50,
-        pos = numberElements;
-
-    /// Exercise 23 variables - Insert element into ascendingly ordered vector without altering the order.
-    int testVec2[10] = {10, 20, 30, 40},
-        numberElements2 = 4;
-    int elem2;
+//    int testVec[TAM_VEC] = {10, 20, 30, 40},
+//        numberElements = 4;
+//    int elem = 50,
+//        pos = numberElements;
+//
+//    /// Exercise 23 variables - Insert element into ascendingly ordered vector without altering the order.
+//    int testVec2[10] = {10, 20, 30, 40},
+//        numberElements2 = 4;
+//    int elem2;
 
     /// Exercise 22
-    printf("testVec[]: \n\n");
-    printf("Cant. elements: %d\n\n", numberElements);
-    printArray(testVec, numberElements, TAM_VEC);
-
-    insertElementInto(testVec, elem, pos, &numberElements, TAM_VEC);
-
-    printf("\n\n");
-    printf("Cant. elements: %d\n\n", numberElements);
-    printArray(testVec, numberElements + 1, TAM_VEC + 1);
+//    printf("testVec[]: \n\n");
+//    printf("Cant. elements: %d\n\n", numberElements);
+//    printArray(testVec, numberElements, TAM_VEC);
+//
+//    insertElementInto(testVec, elem, pos, &numberElements, TAM_VEC);
+//
+//    printf("\n\n");
+//    printf("Cant. elements: %d\n\n", numberElements);
+//    printArray(testVec, numberElements + 1, TAM_VEC + 1);
 
     /// ############################################ ///
 
     /// Exercise 23
-    printf("testVec2[]: \n\n");
-    printArray(testVec2, numberElements2 + 1, TAM_VEC + 1);
-
-    elem2 = 5;
-    insertElementIntoOrderedVector(testVec2, elem2, &numberElements2, TAM_VEC);
-
-    printf("\n\ntestVec2[] inserting %d: \n\n", elem2);
-    printArray(testVec2, numberElements2 + 1, TAM_VEC + 1);
+//    printf("testVec2[]: \n\n");
+//    printArray(testVec2, numberElements2 + 1, TAM_VEC + 1);
+//
+//    elem2 = 5;
+//    insertElementIntoOrderedVector(testVec2, elem2, &numberElements2, TAM_VEC);
+//
+//    printf("\n\ntestVec2[] inserting %d: \n\n", elem2);
+//    printArray(testVec2, numberElements2 + 1, TAM_VEC + 1);
 
     /// ############################################ ///
     /// Strings
+    char orig[] = "world";
+    char dest[SIZE] = "hello";
+    unsigned int destChars = str_len(dest),
+                 origChars = str_len(orig);
+
+    printf("dest: %s -> %d\n", dest, destChars);
+    printf("orig: %s -> %d\n", orig, origChars);
+
+    str_cat(dest, orig);
+    printf("%s (%d)", dest, str_len(dest));
 
     return 0;
 }
