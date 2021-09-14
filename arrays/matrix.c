2243883d8e575a90739abd1fe56fe021f65d6c5e
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-09-14 16:16:24 -0300

    feat: minor changes and upperTriangular() function developed.

--- arrays/matrix.c
+++ arrays/matrix.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include "matrix.h"
 
+int showElementOfArray(int* mat, int numberRows, int numberColumns, int row, int col) {
+    return *(mat + (row * numberColumns) + col);
+}
+
 void mainDiagonal(int mat[ROWS][COLS], int numberRows, int numberColums) {
     int i;
 
@@ -16,14 +20,25 @@ void mainDiagonal(int mat[ROWS][COLS], int numberRows, int numberColums) {
 
 void secondaryDiagonal(int mat[ROWS][COLS], int numberRows, int numberColums) {
     int i;
-    int nRows = numberRows - 1;
+    int nCols = numberColums - 1;
 
     if (numberRows != numberColums) {
         printf("Your matrix is not square.\n");
         return;
     }
 
-    for (i = nRows; i >= 0; i--) {
+    for (i = nCols; i >= 0; i--) {
         printf("mat[%d][%d] = %d\n", i, i, mat[i][i]);
+    }
+}
+
+void upperTriangular(int mat[ROWS][COLS], int numberRows, int numberColums) {
+    int i, j;
+
+    for(i = 0; i < numberRows - 1; i++) {
+        for(j = 0; j < numberColums - 1 - i; j++) {
+            printf("%d\t", i, j, mat[i][j]);
+        }
+        printf("\n");
     }
 }
