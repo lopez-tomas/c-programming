65f6d8058f623ec258198230290b044eec7949e9
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-09-14 15:17:27 -0300

    feat: showElementOfArray(), upperTriangular() & lowerTriangular() function prototypes added.

--- arrays/matrix.h
+++ arrays/matrix.h
@@ -4,7 +4,10 @@
 #define ROWS 5
 #define COLS 5
 
+int showElementOfArray(int* mat, int numberRows, int numberColumns, int row, int col);
 void mainDiagonal(int mat[ROWS][COLS], int numberRows, int numberColums);
 void secondaryDiagonal(int mat[ROWS][COLS], int numberRows, int numberColums);
+void upperTriangular(int mat[ROWS][COLS], int numberRows, int numberColums);
+void lowerTriangular(int mat[ROWS][COLS], int numberRows, int numberColums);
 
 #endif // MATRIX_H
