ccb957f10cf08969cf2cb6af3c8a3839e15dacae
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-09-14 16:17:03 -0300

    minor changes. Test of upperTriangular() function added.

--- arrays/main.c
+++ arrays/main.c
@@ -4,13 +4,15 @@
 
 int main() {
 
-    int mat[ROWS][COLS] = { {1,2,3,4,5}, {1,2,3,4,5}, {1,2,3,4,5} };
+    int mat[ROWS][COLS] = { {1,2,3}, {1,2,3}, {1,2,3} };
     int numberRows = 3,
         numberColums = 3;
 
     mainDiagonal(mat, numberRows, numberColums);
     printf("\n\n");
     secondaryDiagonal(mat, numberRows, numberColums);
+    printf("\n\n");
+    upperTriangular(mat, numberRows, numberColums);
 
     return 0;
 }
