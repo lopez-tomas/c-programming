aee8b5dba79fa96a06bd21cdd0566593af786cc3
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-09-14 14:14:23 -0300

    fix: Using only one variable to loop through array.

--- arrays/matrix.c
+++ arrays/matrix.c
@@ -9,22 +9,21 @@ void mainDiagonal(int mat[ROWS][COLS], int numberRows, int numberColums) {
         return;
     }
 
-    for (i = 0; i < numberRows && i < numberColums; i++) {
+    for (i = 0; i < numberRows; i++) {
         printf("mat[%d][%d] = %d\n", i, i, mat[i][i]);
     }
 }
 
 void secondaryDiagonal(int mat[ROWS][COLS], int numberRows, int numberColums) {
-    int i, j;
-    int nRows = numberRows - 1,
-        nCols = numberColums - 1;
+    int i;
+    int nRows = numberRows - 1;
 
     if (numberRows != numberColums) {
         printf("Your matrix is not square.\n");
         return;
     }
 
-    for (i = nRows, j = nCols; i >= 0 && j >= 0; i--, j--) {
+    for (i = nRows; i >= 0; i--) {
         printf("mat[%d][%d] = %d\n", i, i, mat[i][i]);
     }
 }
