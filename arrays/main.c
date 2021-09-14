86e03c3cb1bf60d1f0fb41b12e4b451e4214e0f9
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-09-14 09:19:23 -0300

    feat: main.h included and testing main & secondary diagonals functions.

--- arrays/main.c
+++ arrays/main.c
@@ -1,11 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
-#define FIL 5
-#define COL 5
+#include "main.h"
+#define ROWS 5
+#define COLS 5
 
 int main() {
 
-    int mat[FIL][COL];
+    int mat[ROWS][COLS] = { {1,2,3,4,5}, {1,2,3,4,5}, {1,2,3,4,5}, {1,2,3,4,5}, {1,2,3,4,5} };
+    int numberRows = 5,
+        numberColums = 5;
+
+    mainDiagonal(mat, numberRows, numberColums);
+    printf("\n\n");
+    secondaryDiagonal(mat, numberRows, numberColums);
 
     return 0;
 }
