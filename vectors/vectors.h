790dfec5847bfdecb48ac97535cd4e5ade98350a
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-09-16 01:21:45 -0300

    feat: eliminateElement() function prototype added.

--- vectors/vectors.h
+++ vectors/vectors.h
@@ -5,6 +5,7 @@ void printArray(int* vec, int posAct, int tam);
 int fillArray(int* vec, int posAct, int tam);
 int insertElementInto(int* vec, int element, int pos, int* posAct, int tam);
 void insertElementIntoOrderedVector(int* vec, int element, int* posAct, int tam);
+void eliminateElement(int* vec, int element, int* posAct, int tam);
 int disjointVectors(int* vec1, const int numberElementsVec1, int* vec2, const int numberElementsVec2);
 int symmetricVector(int* vec, const int numElements);
 
