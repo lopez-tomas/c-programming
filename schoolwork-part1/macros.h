70cf0373891086f988ade236a032bcf910a808dc
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-09-20 16:12:47 -0300

    feat: IS_GREEK_LETTER(c) macro added.

--- schoolwork-part1/macros.h
+++ schoolwork-part1/macros.h
@@ -12,6 +12,11 @@
                                 ( INT_CAST_FOR_CHAR(c) >= 160 && INT_CAST_FOR_CHAR(c) <= 165 ) \
                               )
 
+#define IS_GREEK_LETTER(c) ( ( INT_CAST_FOR_CHAR(c) >= 224 && INT_CAST_FOR_CHAR(c) <= 232 ) || \
+                               ( INT_CAST_FOR_CHAR(c) == 234 || INT_CAST_FOR_CHAR(c) == 235 ) || \
+                               ( INT_CAST_FOR_CHAR(c) == 237 || INT_CAST_FOR_CHAR(c) == 238 ) \
+                             )
+
 #define IS_LETTER(c) ( IS_MAYUS(c) || IS_MINUS(c) || IS_ACCENTED_LETTER(c) )
 #define IS_WHITE(c) (c) == ' '
 #define TO_MAYUS(c) ( ( (c) >= 'a' && (c) <= 'z' ) ? ( ( (c) - 'a' ) + 'A' ) : (c) )
