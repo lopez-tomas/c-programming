eb6c0d0e3dd804d4c8e470ada5f67bf7eacfe319
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-09-24 14:17:16 -0300

    feat: struct tStudents created and testing function to load Students lot.

--- archives/main.c
+++ archives/main.c
@@ -4,11 +4,23 @@
 #include "main.h"
 #define LINE_DIM 80
 
+typedef struct {
+    int dni;
+    char fullName[100];
+    char department;
+    float promedy;
+}tStudents;
+
 int main() {
     FILE* pArchive;
     char fileName[] = "archives/test.txt";
     char mode[] = "rt";
     char line[LINE_DIM];
+
+    tStudents student[10];
+    FILE* pStudents;
+    char studentsFile[] = "archives/students.txt";
+    char lineStudent[200];
 
 //    FILE* pf = fopen("archives/test.txt", "rt");
 //    if (!pf) {
@@ -22,18 +34,35 @@ int main() {
 //
 //    fclose(pf);
 
-    if ( openFile(&pArchive, fileName, mode) ) {
-        printf("%s was opened.\n\n", fileName);
-        printf("Content:\n");
+//    if ( openFile(&pArchive, fileName, mode) ) {
+//        printf("%s was opened.\n\n", fileName);
+//        printf("Content:\n");
+//
+//        while( fgets(line, LINE_DIM - 1, pArchive) ) {
+//            printf("%s", line);
+//        }
+//    } else {
+//        printf("%s could not be opened.", fileName);
+//        return 100;
+//    }
+//    fclose(pArchive);
 
-        while( fgets(line, LINE_DIM - 1, pArchive) ) {
-            printf("%s", line);
-        }
-    } else {
-        printf("%s could not be opened.", fileName);
-        return 100;
+//    printf("\n\n%d %d %d %d \nTotal = %d", sizeof(student.dni), sizeof(student.fullName), sizeof(student.department), sizeof(student.promedy), sizeof(student));
+
+    if (loadStudentsTestLot(studentsFile) == 0) {
+        printf("The file could not be created.");
+        return 300;
     }
-    fclose(pArchive);
+
+    printf("The file was created succesfully.");
+
+    pStudents = fopen(studentsFile, "rt");
+
+    while (fgets(line, 199, pStudents)) {
+        sscanf(lineStudent, "%d|%[^|]|%c|%f", &student.dni, student.fullName, &student.department, &student.promedy);
+        fprintf(stdout, "%d|%s|%c|%f", student.dni, student.fullName, student.department, student.promedy);
+    }
+    fclose(pStudents);
 
     return 0;
 }
