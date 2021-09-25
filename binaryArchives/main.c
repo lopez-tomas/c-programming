bf9073325327b1580fbac55593f3ba8a3ca7ec72
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-09-24 21:03:31 -0300

    feat: testing uncompleted.

--- binaryArchives/main.c
+++ binaryArchives/main.c
@@ -16,7 +16,12 @@ int main() {
     createTestProfessorsLot(employeesFileName);
     createTestNoveltiesLot(noveltiesFileName);
 
-    FILE* pEmp, pNov, pTemp, pErr;
+    FILE* pEmp, *pNov, *pTemp, *pErr;
+    tProfessors emp;
+    tNovelties nov;
+    tProfessors temp;
+    tNovelties err;
+    int cmp;
 
     pEmp = fopen(employeesFileName, "rb");
     pNov = fopen(noveltiesFileName, "rb");
@@ -28,13 +33,28 @@ int main() {
         return -1;
     }
 
+    fread(&emp, sizeof(tEmployees), 1, pEmp);
+    fread(&nov, sizeof(tNovelties), 1, pNov);
     while ( !feof(pEmp) || !feof(pNov) ) {
-        if () {
+        cmp = compareEmployees(&emp, &nov.prof); // -> I must always pass structs by memory direction
 
-        } else if () {
+        if ( !cmp ) { // -> if( cmp == 0 )
+            if ( nov.novelty == 'r' );
+                fread(&emp, sizeof(tEmployees), 1, pEmp);
+                fread(&nov, sizeof(tNovelties), 1, pNov);
 
+            } else if ( nov.novelty == 'm' ) {
+                fwrite(&nov.prof, sizeof(tEmployees), 1, pTemp);
+                /*fflush(pTemp);*/
+
+            } else {
+                fwrite(&nov, sizeof(tNovelties), 1, pErr); // change it to use fprintf (must change extension of error file to be a text)
+                /*fflush(pErr);*/
+            }
         } else {
+            if ( nov.novelty == 'a' ) {
 
+            }
         }
     }
 
