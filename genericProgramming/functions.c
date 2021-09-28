b57b128f750d8102ca61b2f5cb27e59cb3b979de
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-09-28 16:56:28 -0300

    feat: exchange() function developed.:

--- genericProgramming/functions.c
+++ genericProgramming/functions.c
@@ -14,3 +14,18 @@ void *mem_move(void *dest, const void *orig, size_t n) {
 
     return dest;
 }
+
+void intercambio(void *s1, void *s2, size_t n) {
+    int i = 0;
+    char aux;
+
+    while (i < n) {
+       aux = *(char*)s2; 
+       *(char*)s2 = *(char*)s1;
+       *(char*)s1 = aux;
+
+       s1++;
+       s2++;
+       i++; 
+    }
+}
--- genericProgramming/functions.h
+++ genericProgramming/functions.h
@@ -3,6 +3,12 @@
 
 #include <stddef.h>
 
+typedef struct {
+	unsigned dni;
+	char apyn[100];
+} tAlumnos;
+
 void *mem_move(void *dest, const void *orig, size_t n);
+void intercambio(void *s1, void *s2, size_t n);
 
 #endif // FUNCTIONS_H
