dadc317920af3ca07247ea51f4afbef0343613c3
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-09-28 16:56:49 -0300

    feat: testis exchange() function.

--- genericProgramming/main.c
+++ genericProgramming/main.c
@@ -16,24 +16,38 @@ int main()
     int cantElem;
     int i;
 
-    do {
-        printf("Cuantos enteros quiere copiar?\n");
-        scanf(" %d", &cantElem);
-    } while (cantElem > tamMAX);
+    /*do {*/
+        /*printf("Cuantos enteros quiere copiar?\n");*/
+        /*scanf(" %d", &cantElem);*/
+    /*} while (cantElem > tamMAX);*/
 
-    memmove(vecDest, vecOri, cantElem * tamMAX);
-    printf("\nmemmove() function:\n");
+    /*memmove(vecDest, vecOri, cantElem * tamMAX);*/
+    /*printf("\nmemmove() function:\n");*/
 
-    tamDest = cantElem;
-    for(i = 0; i < tamDest; i++) {
-        printf("%d\n", vecDest[i]);
+    /*tamDest = cantElem;*/
+    /*for(i = 0; i < tamDest; i++) {*/
+        /*printf("%d\n", vecDest[i]);*/
+    /*}*/
+
+    /*mem_move(vecDest2, vecOri, cantElem * tamMAX);*/
+    /*printf("\nMy memmove() function:\n");*/
+    /*for(i = 0; i < tamDest; i++) {*/
+        /*printf("%d\n", vecDest[i]);*/
+    /*}*/
+
+    int vec[] = {1,2,3,4};
+    int vec2[] = {5,6,7,8};
+    intercambio(vec, vec2, sizeof(vec));
+
+    for(i = 0; i < sizeof(vec) / 4; i++) {
+        printf("%d\t%d\n", vec[i], vec2[i]);
     }
 
-    mem_move(vecDest2, vecOri, cantElem * tamMAX);
-    printf("\nMy memmove() function:\n");
-    for(i = 0; i < tamDest; i++) {
-        printf("%d\n", vecDest[i]);
-    }
-    
+    tAlumnos alu1 = {111111, "Carlos Tovarez"};
+    tAlumnos alu2;
+    intercambio(&alu1, &alu2, sizeof(tAlumnos));
+
+    printf("\n\n%u %s\n", alu2.dni, alu2.apyn);
+
     return 0;
 }
