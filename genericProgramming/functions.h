6d3042192dc5c2982d2ce27d5c1986e47ae60bef
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-09-29 19:51:36 -0300

    feat: showInteger(), showFloat() & showVector() function prototypes added.

--- genericProgramming/functions.h
+++ genericProgramming/functions.h
@@ -11,4 +11,9 @@ typedef struct {
 void *mem_move(void *dest, const void *orig, size_t n);
 void swap(void *s1, void *s2, size_t n);
 
+void showInteger(void* vec);
+void showFloat(void* vec);
+
+void showVector(void* buffer, size_t element, size_t numberElements, void act(void*));
+
 #endif // FUNCTIONS_H
