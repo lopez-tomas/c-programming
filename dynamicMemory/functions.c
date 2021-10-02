2739d1d29b152068ded90ee8b42d734d4bde6422
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-10-01 22:35:28 -0300

    feat: special_strcpy() function developed.

--- dynamicMemory/functions.c
+++ dynamicMemory/functions.c
@@ -2,3 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include "functions.h"
+
+char* special_strcpy(char* dest, char* orig) {
+    int numChars = strlen(orig) + 1;
+
+    dest = malloc(numChars);
+
+    return dest == NULL ? NULL : strcpy(dest, orig);
+}
--- dynamicMemory/functions.h
+++ dynamicMemory/functions.h
@@ -1,5 +1,6 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
+char* special_strcpy(char* dest, char* orig);
 
 #endif // FUNCTIONS_H
