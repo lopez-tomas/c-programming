238e040255fb22453d62ec72f5cb3016f433200b
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-10-05 18:35:40 -0300

    feat: testing Pile & Node.

--- stacks/main.c
+++ stacks/main.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
-int main()
-{
-    printf("Hello world!\n");
+int main() {
+    int vec[] = {1, 3, 2, 4};
+    int i;
+    t_Stack stack;
+    int data;
+
+    createStack(&stack);
+
+    for(i = 0; i < 4; i++) {
+        push(&stack, &vec[i], sizeof(int));
+    }
+    if (top(&stack, &data, sizeof(int))) {
+        printf("Top: %d\n\n", data);
+    }
+
+    while(pop(&stack, &data, sizeof(int))) {
+        printf("%d\t", data);
+    }
+
+    printf("\n\nDone!\n");
     return 0;
 }
