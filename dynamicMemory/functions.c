12ea5e11b0ddfd3f0ff6deae770c4fc4d86d472d
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-10-06 16:12:31 -0300

    feat: minor changes on special_strcpy() function & mem_move() function commented because it's not working yet.

--- dynamicMemory/functions.c
+++ dynamicMemory/functions.c
@@ -6,11 +6,38 @@
 char* special_strcpy(char* dest, const char* orig) {
     int numChars = strlen(orig) + 1;
 
-    dest = calloc(numChars, char);
+    dest = calloc(numChars, sizeof(char));
 
-    return dest == NULL ? NULL : strcpy(dest, orig);
+    return (dest == NULL ? NULL : strcpy(dest, orig));
 }
 
-void* mem_move(void* s1, const void* s2, size_t numberElements) {
-    return s1;
-}
+/*void* mem_move(void* s1, const void* s2, size_t numberBytes) {*/
+    /*void* initialPos = s1;*/
+    /*void* aux;*/
+    /*int numChars = strlen(s2) + 1;*/
+    /*int i;*/
+
+    /*aux = calloc(numChars, sizeof(char));*/
+    /*if(aux == NULL) {*/
+        /*free(aux);*/
+        /*return NULL;*/
+    /*}*/
+    /*strcpy(aux, s2);*/
+
+    /*[>s1 = calloc(numChars, sizeof(char));<]*/
+    /*[>if(s1 == NULL) {<]*/
+        /*[>free(aux);<]*/
+        /*[>free(s1);<]*/
+        /*[>return NULL;<]*/
+    /*[>}<]*/
+
+    /*for(i = 0; i < numberBytes; i++) {*/
+        /**(char*)s1 = *(char*)aux;*/
+
+        /*aux++;*/
+        /*s1++;*/
+    /*}*/
+    /*[>free(aux);<]*/
+
+    /*return initialPos;*/
+/*}*/
