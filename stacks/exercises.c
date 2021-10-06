21443df59866fd89c93d28bd279c4507e35762d2
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-10-06 20:19:25 -0300

    feat: isMathExpressionCorrect() function developed.

--- stacks/exercises.c
+++ stacks/exercises.c
@@ -12,3 +12,31 @@
 
 #define isOpenness(c) ( isOpeningParenthesis(c) || isOpeningBracket(c) || isOpeningBrace(c) )
 #define isClosure(c) ( isClosingParenthesis(c) || isClosingBracket(c) || isClosingBrace(c) )
+
+int isMathExpressionCorrect(const char* mathExpression) {
+    t_Stack stack;
+    char obj;
+
+    createStack(&stack);
+
+    while(*mathExpression) {
+        if ( isOpenness(*mathExpression) ) {
+            push(&stack, mathExpression, sizeof(char));
+        }
+
+        if ( isClosure(*mathExpression) ) {
+            if (isEmpty(&stack)) {
+                return 0;
+            }
+
+            pop(&stack, &obj, sizeof(char));
+            if ( isClosingParenthesis(*mathExpression) && !isOpeningParenthesis(obj) ) { return 0; }
+            if ( isClosingBracket(*mathExpression) && !isOpeningBracket(obj) ) { return 0; }
+            if ( isClosingBrace(*mathExpression) && !isOpeningBrace(obj) ) { return 0; }
+        }
+        mathExpression++;
+    }
+    if (!isEmpty(&stack)) { return 0; }
+
+    return 1;
+}
