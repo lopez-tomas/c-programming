c8381a30a2572320e717d073587bff293bff5684
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-10-06 16:10:19 -0300

    feat: mem_move(), createIntegerMatrix(), fillIntegerMatrix() & showIntegerMatrix() functions prototypes added.

--- dynamicMemory/functions.h
+++ dynamicMemory/functions.h
@@ -2,6 +2,10 @@
 #define FUNCTIONS_H
 
 char* special_strcpy(char* dest, const char* orig);
-void* mem_move(void* s1, const void* s2, size_t numberElements);
+void* mem_move(void* s1, const void* s2, size_t numberBytes);
+
+int** createIntegerMatrix(unsigned numberFiles, unsigned numberColumns);
+void fillIntegerMatrix(int** matrix, unsigned numberFiles, unsigned numberColumns);
+void showIntegerMatrix(int** matrix, unsigned numberFiles, unsigned numberColumns);
 
 #endif // FUNCTIONS_H
