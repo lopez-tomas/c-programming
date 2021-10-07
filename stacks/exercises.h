d54432cf7de2bab2c36e8551c9464c534b204f6b
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-10-07 00:47:42 -0300

    feat: addTwoNumbers() function prototype added.

--- stacks/exercises.h
+++ stacks/exercises.h
@@ -4,5 +4,6 @@
 #include "./dynamic/Stack.h"
 
 int isMathExpressionCorrect(const char* mathExpression);
+void addTwoNumbers(const char* numberA, const char* numberB, t_Stack* result);
 
 #endif // EXERCISES_H
