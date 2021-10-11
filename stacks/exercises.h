ccad5acbaafb1ba19c09da686e7e0aa55d278ae0
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-10-11 13:15:10 -0300

    feat: stacking() function prototype added.

--- stacks/exercises.h
+++ stacks/exercises.h
@@ -4,6 +4,7 @@
 #include "./dynamic/Stack.h"
 
 int isMathExpressionCorrect(const char* mathExpression);
+int stacking(const char* number, t_Stack* stack);
 void addTwoNumbers(const char* numberA, const char* numberB, t_Stack* result);
 
 #endif // EXERCISES_H
