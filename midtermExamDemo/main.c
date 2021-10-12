dcdc7b9902d731d1ed6149987297108c04573383
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-10-12 13:14:00 -0300

    feat: minor changes.

--- midtermExamDemo/main.c
+++ midtermExamDemo/main.c
@@ -6,6 +6,10 @@ int main() {
     char decrypt_filename[] = "archives/desofuscado.txt";
     int wasDecrypted;
 
+    puts("##########################");
+    puts("Midterm Exam Demo - Part 1");
+    puts("##########################\n");
+
     puts("Encrypted file:\n");
     readTextFile(encrypt_filename);
     puts("\n");
@@ -13,13 +17,17 @@ int main() {
     wasDecrypted = decryptFile(encrypt_filename, encrypt_file__opening_mode, decrypt_filename);
 
     if( wasDecrypted == SUCCESS) {
-        printf("%s file decrypted. You can read it at %s\n\n\n", encrypt_filename, decrypt_filename);
+        puts("#################################################################################");
+        printf("%s file decrypted. You can read it at %s\n", encrypt_filename, decrypt_filename);
+        puts("#################################################################################\n\n");
 
         puts("Decrypted file:\n");
         readTextFile(decrypt_filename);
         puts("");
     } else {
-        printf("%s file could not be decrypted.\n\n\n", encrypt_filename);
+        puts("##################################################");
+        printf("%s file could not be decrypted.\n", encrypt_filename);
+        puts("##################################################\n");
 
         return ERR__DECRYPTING_FILE;
     }
