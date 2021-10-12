fb9e3936908e68a65206334b179066748e382d5a
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-10-12 12:26:57 -0300

    feat: codes of success & errors added. decryptFile() function prototype modified.

--- midtermExamDemo/archives.h
+++ midtermExamDemo/archives.h
@@ -1,6 +1,11 @@
 #ifndef ARCHIVES_H
 #define ARCHIVES_H
 
+#define ERR 0
+#define SUCCESS 100
+#define ERR__ENCRYPTED_FILE 200
+#define ERR__DECRYPTED_FILE 300
 
+int decryptFile(const char* encrypted_filename, const char* opening_mode__encrypted_file, const char* decrypted_filename);
 
 #endif // ARCHIVES_H
