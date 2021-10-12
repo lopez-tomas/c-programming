01c075926f3857a228ae182e48e6e5301f3a5788
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-10-12 12:27:27 -0300

    feat: functions added.

--- midtermExamDemo/decryption.c
+++ midtermExamDemo/decryption.c
@@ -1,3 +1,80 @@
 #include "decryption.h"
 #include "stringFunctions.h"
 #include "macros.h"
+
+char* nextWord(char* source, unsigned* wordLength) {
+    unsigned letters = 0;
+    char* wordStart;
+
+    while(*source && !IS_LETTER(*source)) {
+        source++;
+    }
+    wordStart = source;
+
+    while(*source && IS_LETTER(*source)) {
+        letters++;
+        source++;
+    }
+    *wordLength = letters;
+
+    return wordStart;
+}
+
+void displaceWord(char* wordStart, char* wordEnd, const char* group, int displacement) {
+    unsigned group_size = str_len((char*)group);
+    char* group_start = (char*)group;
+    char* group_aux = group_start;
+    char* letter;
+
+    while(wordStart <= wordEnd) {
+        if(isLetterInGroup(*wordStart, group)) {
+            letter = str_chr((char*)group, *wordStart);
+
+            group_aux += ABS(letter - group_start + displacement) % group_size;
+            *wordStart = *group_aux;
+            group_aux = group_start;
+        }
+
+        wordStart++;
+    }
+}
+
+int isLetterInGroup(char letter, const char* group) {
+    return (str_chr((char*)group, letter)) ? 1 : 0;
+}
+
+void invertWord(char* wordStart, char* wordEnd) {
+    while(wordStart < wordEnd) {
+        swapCharacters(wordStart, wordEnd);
+
+        wordStart++;
+        wordEnd--;
+    }
+}
+
+void swapCharacters(char* letter1, char* letter2) {
+    char aux = *letter1;
+    *letter1 = *letter2;
+    *letter2 = aux;
+}
+
+char* decryptionWithDisplacement(char* source, const char* group, int displacement, unsigned isInverted) {
+    char* wordStart;
+    char* wordEnd = source;
+    unsigned wordLength;
+
+    while(*wordEnd) {
+        wordStart = nextWord(wordEnd, &wordLength);
+        wordEnd = wordStart + wordLength - 1;
+
+        displaceWord(wordStart, wordEnd, group, displacement);
+
+        if(isInverted) {
+            invertWord(wordStart, wordEnd);
+        }
+
+        wordEnd++;
+    }
+
+    return source;
+}
--- midtermExamDemo/decryption.h
+++ midtermExamDemo/decryption.h
@@ -1,5 +1,11 @@
 #ifndef DECRYPTION_H
 #define DECRYPTION_H
 
+char* nextWord(char* source, unsigned* wordLength);
+void displaceWord(char* wordStart, char* wordEnd, const char* group, int displacement);
+int isLetterInGroup(char letter, const char* group);
+void invertWord(char* wordStart, char* wordEnd);
+void swapCharacters(char* letter1, char* letter2);
+char* decryptionWithDisplacement(char* source, const char* group, int displacement, unsigned isInverted);
 
 #endif // DECRYPTION_H
