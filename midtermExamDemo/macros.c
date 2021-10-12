15fc9a9740c93368b42129a2a28ce12db599d33f
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-10-12 11:52:36 -0300

    feat: macros.c file added with functions.

---
+++ midtermExamDemo/macros.c
@@ -0,0 +1,13 @@
+#include "macros.h"
+
+int toAbsoluteValue(int x) {
+    return ABS(x);
+}
+
+int returnIntPart(float x) {
+    return INT_PART(x);
+}
+
+float returnDecimalPart(float x) {
+    return DECIMAL_PART(x);
+}
