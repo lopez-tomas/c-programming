c71452f5a14370524ff7eb3de0559c639591d59e
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-10-15 14:10:35 -0300

    feat: commenting include of dynamic/Stack.h

--- stacks/main.h
+++ stacks/main.h
@@ -1,7 +1,8 @@
 #ifndef MAIN_H_INCLUDED
 #define MAIN_H_INCLUDED
 
-#include "dynamic/Stack.h"
+//#include "dynamic/Stack.h"
+#include "static/Stack.h"
 #include "exercises.h"
 
 #endif // MAIN_H_INCLUDED
