e45979419e2b85c1e798a3cafa36e4221e85c636
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-10-15 15:48:45 -0300

    feat: createQueue() function implemented.

--- queues/dynamic/Queue.c
+++ queues/dynamic/Queue.c
@@ -1,1 +1,13 @@
 #include "Queue.h"
+
+void createQueue(t_Queue* queue) {
+	queue->front = NULL;
+	queue->rear = NULL;
+}
+
+/*int push(t_Queue* queue, const void* data, size_t data_size);*/
+/*int pop(t_Queue* queue, void* data, size_t data_size);*/
+/*int front(const t_Queue* queue, void* data, size_t data_size);*/
+/*int isEmpty(const t_Queue* queue);*/
+/*int isFull(const t_Queue* queue, size_t data_size);*/
+/*void emptyQueue(t_Queue* queue);*/
