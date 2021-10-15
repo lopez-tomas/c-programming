3653c9edbc150522179721b8aa2aaa1cc4d34823
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-10-15 15:45:58 -0300

    feat: primitives' prototypes for Dynamic Queue were added.

--- queues/dynamic/Queue.h
+++ queues/dynamic/Queue.h
@@ -4,8 +4,8 @@
 #include "stdlib.h"
 
 typedef struct s_Node {
-	void* data;
-	size_t data_size;
+	void* info;
+	size_t info_size;
 	struct s_Node* next;
 } t_Node;
 
@@ -13,5 +13,13 @@ typedef struct {
 	t_Node* front;
 	t_Node* rear;
 } t_Queue;
+
+void createQueue(t_Queue* queue);
+int push(t_Queue* queue, const void* data, size_t data_size);
+int pop(t_Queue* queue, void* data, size_t data_size);
+int front(const t_Queue* queue, void* data, size_t data_size);
+int isEmpty(const t_Queue* queue);
+int isFull(const t_Queue* queue, size_t data_size);
+void emptyQueue(t_Queue* queue);
 
 #endif // QUEUE_H
