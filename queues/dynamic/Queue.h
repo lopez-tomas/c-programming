1ff93c1b9d5dd19564a7bec9bee189a0557bd6ad
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-10-15 15:27:49 -0300

    feat: structs for node and queue created.

--- queues/dynamic/Queue.h
+++ queues/dynamic/Queue.h
@@ -1,6 +1,17 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
+#include "stdlib.h"
 
+typedef struct s_Node {
+	void* data;
+	size_t data_size;
+	struct s_Node* next;
+} t_Node;
+
+typedef struct {
+	t_Node* front;
+	t_Node* rear;
+} t_Queue;
 
 #endif // QUEUE_H
