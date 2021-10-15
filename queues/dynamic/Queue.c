de85853c1aad361f9c4d957d427fbe4d82cb9a4f
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-10-15 16:04:43 -0300

    feat: isEmpty() & isFull() functions implemented.

--- queues/dynamic/Queue.c
+++ queues/dynamic/Queue.c
@@ -8,6 +8,27 @@ void createQueue(t_Queue* queue) {
 /*int push(t_Queue* queue, const void* data, size_t data_size);*/
 /*int pop(t_Queue* queue, void* data, size_t data_size);*/
 /*int front(const t_Queue* queue, void* data, size_t data_size);*/
-/*int isEmpty(const t_Queue* queue);*/
-/*int isFull(const t_Queue* queue, size_t data_size);*/
+
+int isEmpty(const t_Queue* queue) {
+	return queue->front == NULL;
+}
+
+int isFull(const t_Queue* queue, size_t data_size) {
+	t_Node* testNode = malloc(sizeof(t_Node));
+	if( !testNode ) {
+		return 1;
+	}
+
+	testNode->info = malloc(sizeof(data_size));
+	if( !testNode->info ) {
+		free(testNode);
+		return 1;
+	}
+
+	free(testNode->info);
+	free(testNode);
+
+	return 0;
+}
+
 /*void emptyQueue(t_Queue* queue);*/
