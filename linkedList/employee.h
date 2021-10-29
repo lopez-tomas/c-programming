54270e467684d4a6f57a473951af65dfc17c9a0d
lopeztomas <lopeztomasenrique98@gmail.com>
2021-10-29 14:49:09 -0300

    feat: employee.h file with a t_Employee struct with functions prototypes.

---
+++ linkedList/employee.h
@@ -0,0 +1,23 @@
+#ifndef EMPLOYEE_H
+#define EMPLOYEE_H
+
+typedef struct {
+    int file_number;
+    char full_name[50];
+    float salary;
+} t_Employee;
+
+
+int showEmployee(const void* vEmp);
+
+int cmpEmployeeSalaray__asc(const void*,const void*);
+int cmpEmployeeSalaray__desc(const void*,const void*);
+
+int cmpEmployeeFullName__asc(const void*,const void*);
+int cmpEmployeeFullName__desc(const void*,const void*);
+
+int cmpEmployeeFileNumber__asc(const void*,const void*);
+int cmpEmployeeFileNumber__desc(const void*,const void*);
+
+
+#endif // EMPLOYEE_H
