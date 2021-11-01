8c83cf9c88f3d935a39362ba16cf0d6a90f71ab5
lopeztomas <lopeztomasenrique98@gmail.com>
2021-11-01 16:15:50 -0300

    feat: insertSorted() & filter() function prototypes added.

--- linkedList/simply/LinkedList.h
+++ linkedList/simply/LinkedList.h
@@ -11,5 +11,8 @@ typedef t_Node* t_LinkedList;
 
 void createLinkedList(t_LinkedList* list);
 int insertInPosition(t_LinkedList* list, const void* data, size_t data_size, unsigned position);
+int insertSorted(t_LinkedList* list, const void* data, size_t data_size, int cmp(const void*, const void*));
+
+void filter(t_LinkedList* list, int action(const void*));
 
 #endif // LINKEDLIST_H
